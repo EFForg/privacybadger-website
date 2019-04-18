1de0694d6024dcc0b084ebf5527083677545811b
Vivian Brown <vivian@eff.org>
2019-04-18 16:22:37 -0700

    Add browser download js

---
+++ assets/js/download.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+
+  // Detect browser.
+  var browser = 'other';
+  if (navigator && navigator.userAgent) {
+    if (navigator.userAgent.match(/Android/i)) {
+      browser = 'android';
+    }
+    else if (navigator.userAgent.match(/firefox/i)) {
+      browser = 'firefox-on-android';
+    }
+    else if (navigator.userAgent.match(/OPR|opera/i)) {
+      browser = 'opera';
+    }
+    else if (navigator.userAgent.match(/chrome/i)) {
+      browser = 'chrome';
+    }
+    else if (navigator.userAgent.match(/iPhone/i)) {
+      browser = 'iphone';
+    }
+  }
+
+  // Update DOM.
+  var $badgerDownload = $('#badger-download');
+  if (browser == 'iphone' || browser == 'other') {
+    $badgerDownload.addClass('not-supported');
+    $('#browser-not-supported').show();
+
+    if (browser == 'iphone') {
+      $('#browser-not-supported .iphone').show();
+    }
+  } else {
+    var url = $badgerDownload.data(browser + "-url");
+    $badgerDownload.find('a').attr('href', url);
+    $('.other-browser.' + browser).hide();
+
+    if (browser == 'firefox-on-android') {
+      $('#android-beta-warning').show();
+    }
+  }
+});
