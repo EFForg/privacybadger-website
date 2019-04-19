9c660a0c668e313dd7626de8788178c2f04c357c
Vivian Brown <vivian@eff.org>
2019-04-19 13:46:31 -0700

    Add back to top button

---
+++ assets/js/faq.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+  var $topBtn = $('#back-to-faq-top');
+
+  var start = $('#faq-body').offset().top;
+  var end = $('footer').offset().top - $(window).height() - 50;
+  $(document).scroll(function() {
+    var y = $(this).scrollTop();
+    if (y > start && y < end) {
+      $topBtn.fadeIn();
+    } else {
+      $topBtn.fadeOut();
+    }
+  });
+
+  $topBtn.click(function(e) {
+    e.preventDefault();
+    document.getElementById('faq-top').scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
