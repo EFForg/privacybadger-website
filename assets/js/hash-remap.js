7a18c914a521be015eb115f692ed0927cdc6d1df
Alexei <alexeiatyahoodotcom@gmail.com>
2020-07-09 11:51:49 -0400

    Fix some redirects

--- assets/js/hash-remap.js
+++ assets/js/hash-remap.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   if (window.location.hash != "") {
-    var hash = window.location.hash.replace(/%[0-9A-F]{2}/i, '');
+    var hash = window.location.hash.replace(/%[0-9A-F]{2}/ig, '');
 
     {{ range $old, $new := .Site.Data.links }}
     if (hash.startsWith({{ jsonify $old }}))
